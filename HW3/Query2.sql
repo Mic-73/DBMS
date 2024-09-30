fc6bbc8627f666107349ceec05440a67d5a0fd28
Mike W <143733799+Mic-73@users.noreply.github.com>
2024-09-29 22:38:16 -0400

    Delete HW3 directory

--- HW3/Query2.sql
+++
@@ -1,9 +0,0 @@
--- ----------------------------------------------
---  Query 2: 
---  List names and descriptions of products that are not sold.
--- ----------------------------------------------
-
-select p.name as Product_Name, description as Product_Description
-from products p
-left join sell s using (pid)
-where s.pid is null;
--- HW3/Query8.sql
+++
@@ -1,9 +0,0 @@
--- ----------------------------------------------
---  Query 8: 
---  On average, what was the cheapest shipping method used ever?
--- ----------------------------------------------
-
-select Shipping_Method, avg(shipping_cost) as Average_Shipping_Cost
-from orders
-group by shipping_method 
-order by Average_Shipping_Cost asc;
